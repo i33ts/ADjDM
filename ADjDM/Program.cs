98001873ff1ba9727d42c7a94d4b05e65d210a59
i33ts <46345425+i33ts@users.noreply.github.com>
2021-10-31 11:43:14 +0200

    Added ContextMenu Choices

--- ADjDM/Program.cs
+++ ADjDM/Program.cs
@@ -25,21 +25,82 @@ namespace ADjDM
 
             public MyCustomApplicationContext()
             {
+                // Initialize ContextMenuStrip
+                ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+                // User Menu
+                ToolStripMenuItem userMenu = new ToolStripMenuItem();
+                userMenu.Text = "User";
+                userMenu.DropDownItems.Add("Check Password Health");
+                userMenu.DropDownItems.Add("Check Password Strength");
+                userMenu.DropDownItems.Add("Show User Information");
+                userMenu.DropDownItems.Add("Show Local Admin Indicator");
+                // Computer Menu
+                ToolStripMenuItem computerMenu = new ToolStripMenuItem();
+                computerMenu.Text = "Computer";
+                computerMenu.DropDownItems.Add("Check Domain Trust");
+                computerMenu.DropDownItems.Add("Check Windows Health");
+                computerMenu.DropDownItems.Add("Check Disks Health");
+                computerMenu.DropDownItems.Add("List System Information");
+                computerMenu.DropDownItems.Add("List Installed Software");
+                // Services Menu
+                ToolStripMenuItem servicesMenu = new ToolStripMenuItem();
+                servicesMenu.Text = "Services";
+                servicesMenu.DropDownItems.Add("Check Windows Time Health");
+                servicesMenu.DropDownItems.Add("Check Windows Update Health");
+                servicesMenu.DropDownItems.Add("List Warning/Error Application Logs");
+                servicesMenu.DropDownItems.Add("List Error Application Logs");
+                servicesMenu.DropDownItems.Add("List Warning/Error System Logs");
+                servicesMenu.DropDownItems.Add("List Error System Logs");
+                // Policies Menu
+                ToolStripMenuItem policiesMenu = new ToolStripMenuItem();
+                policiesMenu.Text = "Policies";
+                policiesMenu.DropDownItems.Add("List Assigned Policies");
+                policiesMenu.DropDownItems.Add("Check Folder Redirection Health");
+                policiesMenu.DropDownItems.Add("Check USB Policy");
+                policiesMenu.DropDownItems.Add("Check CD Policy");
+                policiesMenu.DropDownItems.Add("Check Encryption Policy");
+                // Connectivity Menu
+                ToolStripMenuItem connectivityMenu = new ToolStripMenuItem();
+                connectivityMenu.Text = "Connectivity";
+                connectivityMenu.DropDownItems.Add("Check Internet Health");
+                    //Connectivity Sub-Menu
+                    ToolStripMenuItem connectivitysubMenu = new ToolStripMenuItem();
+                    connectivitysubMenu.Text = "Check Network Health";
+                    connectivitysubMenu.DropDownItems.Add("NIC disconnects");
+                    connectivitysubMenu.DropDownItems.Add("CRC Errors");
+                    connectivitysubMenu.DropDownItems.Add("Packet Retransmissions");
+                connectivityMenu.DropDownItems.Add(connectivitysubMenu);
+                connectivityMenu.DropDownItems.Add("Enable/Disable Proxy");
+                connectivityMenu.DropDownItems.Add("Concurrent WiFi/Ethernet Indicator");
+                // Assistance Menu
+                ToolStripMenuItem assistanceMenu = new ToolStripMenuItem();
+                assistanceMenu.Text = "Assistance";
+                assistanceMenu.DropDownItems.Add("Live Chat");
+                assistanceMenu.DropDownItems.Add("Send Email");
+                // Add Menus to Main Menu
+                contextMenuStrip.Items.Add(userMenu);
+                contextMenuStrip.Items.Add(computerMenu);
+                contextMenuStrip.Items.Add(servicesMenu);
+                contextMenuStrip.Items.Add(policiesMenu);
+                contextMenuStrip.Items.Add(connectivityMenu);
+                contextMenuStrip.Items.Add(assistanceMenu);
+                contextMenuStrip.Items.Add("Exit", null, this.Exit_Click);
+
                 // Initialize Tray Icon
-                trayIcon = new NotifyIcon() { Icon = Resources.appIcon,
-                Visible = true };
+                trayIcon = new NotifyIcon()
+                {
+                    Icon = Resources.appIcon,
+                    ContextMenuStrip = contextMenuStrip,
+                    Visible = true
+                };
             }
+
+            void Exit_Click(object sender, EventArgs e)
+            {
+                trayIcon.Visible = false;
+                Application.Exit();
+            }
+
         }
-
-        //   mainmenu.Items.Add("Exit");
-
-        //     void Exit(object sender, EventArgs e)
-        //         {
-        // Hide tray icon, otherwise it will remain shown until user mouses over it
-        //             trayIcon.Visible = false;
-
-        //             Application.Exit();
-        //         }
-        //     }
     }
 }
