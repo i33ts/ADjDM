0d5ad6d7bf8c4944f7e6332964c7645a79ec6de6
i33ts <46345425+i33ts@users.noreply.github.com>
2022-12-24 14:44:00 +0200

    Added computer Trust & System Info code
    Also added cmd execution void classes

--- ADjDM/Program.cs
+++ ADjDM/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Windows.Forms;
 using System.Net;
 using System.Drawing;
+using System.Threading;
 
 namespace ADjDM
 {
@@ -40,10 +41,10 @@ namespace ADjDM
                 ToolStripMenuItem computerMenu = new ToolStripMenuItem();
                 computerMenu.Text = "Computer";
                 computerMenu.Image = Resources.computers.ToBitmap();
-                computerMenu.DropDownItems.Add("Check Domain Trust", Resources.domain.ToBitmap());
+                computerMenu.DropDownItems.Add("Check Domain Trust", Resources.domain.ToBitmap(), this.CheckDomainTrust_Click);
                 computerMenu.DropDownItems.Add("Check Windows Health", Resources.windows.ToBitmap());
                 computerMenu.DropDownItems.Add("Check Disks Health", Resources.harddisk.ToBitmap());
-                computerMenu.DropDownItems.Add("List System Information", Resources.info.ToBitmap());
+                computerMenu.DropDownItems.Add("List System Information", Resources.info.ToBitmap(), this.ListSystemInfo_Click);
                 computerMenu.DropDownItems.Add("List Installed Software", Resources.software.ToBitmap());
                 // Services Menu
                 ToolStripMenuItem servicesMenu = new ToolStripMenuItem();
@@ -156,6 +157,88 @@ namespace ADjDM
                 }
             }
 
+            void CheckDomainTrust_Click(object sender, EventArgs e) 
+            {
+                ExecuteCommandSync(@"NLTEST /SC_VERIFY:VIANEX.LOCAL");
+            }
+
+            void ListSystemInfo_Click(object sender, EventArgs e) 
+            {
+                ExecuteCommandSync(@"systeminfo");
+            }
+
+            /// <span class="code-SummaryComment"><summary></span>
+            /// Executes a shell command synchronously.
+            /// <span class="code-SummaryComment"></summary></span>
+            /// <span class="code-SummaryComment"><param name="command">string command</param></span>
+            /// <span class="code-SummaryComment"><returns>string, as output of the command.</returns></span>
+            public void ExecuteCommandSync(object command)
+            {
+                try
+                {
+                    // create the ProcessStartInfo using "cmd" as the program to be run,
+                    // and "/c " as the parameters.
+                    // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                    // and then exit.
+                    System.Diagnostics.ProcessStartInfo procStartInfo =
+                        new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+                    // The following commands are needed to redirect the standard output.
+                    // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                    procStartInfo.RedirectStandardOutput = true;
+                    procStartInfo.RedirectStandardError = true;
+                    procStartInfo.UseShellExecute = false;
+                    // Do not create the black window.
+                    procStartInfo.CreateNoWindow = true;
+                    // Now we create a process, assign its ProcessStartInfo and start it
+                    System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                    proc.StartInfo = procStartInfo;
+                    proc.Start();
+                    // Get the output into a string
+                    string result = proc.StandardOutput.ReadToEnd();
+                    string error = proc.StandardError.ReadToEnd();
+                    // Display the command output.
+                    if (result != String.Empty)
+                        MessageBox.Show(result, "Success Message");
+                    else
+                        MessageBox.Show(error, "Failure Message");
+                }
+                catch (Exception objException)
+                {
+                    // Log the exception
+                }
+            }
+
+            /// <span class="code-SummaryComment"><summary></span>
+            /// Execute the command Asynchronously.
+            /// <span class="code-SummaryComment"></summary></span>
+            /// <span class="code-SummaryComment"><param name="command">string command.</param></span>
+            public void ExecuteCommandAsync(string command)
+            {
+                try
+                {
+                    //Asynchronously start the Thread to process the Execute command request.
+                    Thread objThread = new Thread(new ParameterizedThreadStart(ExecuteCommandSync));
+                    //Make the thread as background thread.
+                    objThread.IsBackground = true;
+                    //Set the Priority of the thread.
+                    objThread.Priority = ThreadPriority.AboveNormal;
+                    //Start the thread.
+                    objThread.Start(command);
+                }
+                catch (ThreadStartException objException)
+                {
+                    // Log the exception
+                }
+                catch (ThreadAbortException objException)
+                {
+                    // Log the exception
+                }
+                catch (Exception objException)
+                {
+                    // Log the exception
+                }
+            }
         }
     }
 }
